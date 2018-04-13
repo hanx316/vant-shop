1305a5a03358c7a7803729ad2267f95068e36f7a
samhanx <xuanxinhan@vip.qq.com>
2018-04-13 23:07:03 +0800

    [feat] add vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import routes from './routes'
+
+Vue.use(VueRouter)
+const router = new VueRouter({ routes }) 
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-});
+})
---
+++ src/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <van-cell-group class="item-list">
+      <van-cell is-link icon="points" title="我的积分" />
+      <van-cell is-link icon="gift" title="我收到的礼物" />
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+import { Cell, CellGroup, Icon } from 'vant';
+
+export default {
+  components: {
+    [Cell.name]: Cell,
+    [CellGroup.name]: CellGroup,
+    [Icon.name]: Icon
+  }
+};
+</script>
+
+<style>
+.van-cell__title .van-icon {
+  font-size: 18px;
+}
+</style>
