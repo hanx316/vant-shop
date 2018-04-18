8c72dec5c62fbe8af0176ffea6d6fd47760cdca7
samhanx <xuanxinhan@vip.qq.com>
2018-04-19 00:33:06 +0800

    [feat] 首页

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <van-list v-model="loading" :finished="finished" @load="onLoad">
-    <van-cell v-for="item in goods" :key="item" :title="item + ''" />
+  <van-list v-model="loading" :finished="finished" @load="onLoad" class="goods-list">
+    <div v-for="item in goods" :key="item.id" class="good-box">
+      <img :src="item.img" class="good-pic">
+      <div class="good-info">
+        <span class="good-name">{{ item.name }}</span>
+        <span class="hot-point">热度：{{ item.hot }}</span>
+      </div>
+    </div>
   </van-list>
 </template>
 
 <script>
-import { Cell, List } from 'vant';
+import { CellGroup, List } from 'vant';
 
 export default {
   data() {
@@ -16,18 +22,25 @@ export default {
     }
   },
   components: {
-    [Cell.name]: Cell,
+    [CellGroup.name]: CellGroup,
     [List.name]: List
   },
   methods: {
     onLoad() {
       setTimeout(() => {
+        console.log('called')
         for (let i = 0; i < 10; i++) {
-          this.goods.push(this.goods.length + 1);
+          let id = this.goods.length + 1
+          this.goods.push({
+            id,
+            name: `商品名称${id}`,
+            hot: `${1000 + id * 10}`,
+            img: 'static/goods.jpeg'
+          });
         }
         this.loading = false;
 
-        if (this.goods.length >= 40) {
+        if (this.goods.length >= 20) {
           this.finished = true;
         }
       }, 500);
@@ -37,7 +50,27 @@ export default {
 </script>
 
 <style>
-.van-cell__title .van-icon {
-  font-size: 18px;
+.goods-list {
+  margin-bottom: 50px;
+  padding: 20px;
+}
+.good-box {
+  height: 6rem;
+  margin-bottom: 10px;
+  padding: 10px 10px 0;
+  background: #fff;
+  box-shadow: 3px 3px 3px #ccc;
+}
+.good-pic {
+  display: block;
+  width: 100%;
+  height: 5rem;
+}
+.good-info {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 1rem;
+  font-size: 16px;
 }
 </style>
