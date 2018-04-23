477d6a8ea8c44658b5134133cb7f0300af8f01f7
samhanx <xuanxinhan@vip.qq.com>
2018-04-23 20:58:00 +0800

    [feat] 夺宝商品页

--- src/pages/GoodTreasure.vue
+++ src/pages/GoodTreasure.vue
@@ -9,25 +9,71 @@
     <div class="good-info-box">
       <div class="good-info">
         <div class="name">{{ name }}</div>
-        <div class="hot">热度：{{ hot }}</div>
       </div>
       <p class="good-desc">{{ desc }}</p>
-      <van-tag v-for="(tag, i) in tags" :key="i" mark type="danger" class="good-tag">{{ tag }}</van-tag>
-    </div>
-    <div class="recommend-good-list">
-      <div class="recommend-title">你可能会喜欢</div>
-      <div class="recommend-goods">
-        <img v-for="(image, i) in recommends" :key="i" :src="image" class="recommend-image">
+      <van-progress :percentage="progress" color="#f44" class="good-progress" />
+      <div class="treasure-info-panel">
+        <div class="treasure-target treasure-info">
+          <span>¥{{ target }}</span>
+          <span>目标金额</span>
+        </div>
+        <div class="treasure-countdown treasure-info">
+          <span>{{ countdown }}天</span>
+          <span>剩余时间</span>
+        </div>
+        <div class="treasure-people treasure-info">
+          <span>{{ support }}</span>
+          <span>支持人数</span>
+        </div>
       </div>
     </div>
-    <van-goods-action>
-      <van-goods-action-big-btn text="立即下注" primary />
+    <van-cell title="往期开奖结果" class="treasure-history" @click="onClickHistory" is-link />
+    <van-steps direction="vertical" >
+      <van-step>
+        <h3>xxxx 参与了</h3>
+        <p>2016-07-12 12:40</p>
+      </van-step>
+      <van-step>
+        <h3>xxxx 参与了</h3>
+        <p>2016-07-11 10:00</p>
+      </van-step>
+      <van-step>
+        <h3>xxxx 参与了</h3>
+        <p>2016-07-10 09:30</p>
+      </van-step>
+    </van-steps>
+    <van-goods-action class="btn-bottom">
+      <van-goods-action-big-btn @click="handleBetClick" text="立即下注" primary />
     </van-goods-action>
+    <van-actionsheet v-model="showPanel" title="选择下注金额" class="bet-panel">
+      <div class="bet-form">
+        <van-cell title="单注金额：" :value="`限额${unitPrice}元`" />
+        <van-cell title="购买数量：">
+          <van-stepper v-model="betCount" />
+        </van-cell>
+        <van-cell title="投注金额：" :value="`${totalPrice}元`" />
+        <van-button size="large" type="danger" @click="submit" class="submit-btn">立即投注</van-button>
+      </div>
+    </van-actionsheet>
   </div>
 </template>
 
 <script>
-import { Swipe, SwipeItem, Tag, GoodsAction, GoodsActionBigBtn } from 'vant'
+import {
+  Swipe,
+  SwipeItem,
+  Tag,
+  GoodsAction,
+  GoodsActionBigBtn,
+  Progress,
+  Cell,
+  Steps,
+  Step,
+  Actionsheet,
+  Stepper,
+  Button,
+  Toast
+} from 'vant'
 import HeaderNav from '@/components/HeaderNav'
 
 export default {
@@ -35,11 +81,20 @@ export default {
     return {
       id: 0,
       name: '商品名称',
-      hot: '1600',
       desc: '这是一段产品描述文字这是一段产品描述文字这是一段产品描述文字这是一段产品描述文字这是一段产品描述文字这是一段产品描述文字这是一段产品描述文字',
-      tags: ['标签1', '标签标签2', '标签3', '标签4', '标签标签标签5', '标签标签6'],
+      progress: 30,
+      target: '10000',
+      countdown: 29,
+      support: 146,
       images: ['static/good1.jpg', 'static/good2.jpg', 'static/good3.jpg', 'static/good4.jpg'],
-      recommends: ['static/goods.jpeg', 'static/goods.jpeg', 'static/goods.jpeg', 'static/goods.jpeg']
+      showPanel: false,
+      unitPrice: 3,
+      betCount: 1
+    }
+  },
+  computed: {
+    totalPrice() {
+      return this.unitPrice * this.betCount
     }
   },
   components: {
@@ -48,7 +103,28 @@ export default {
     [SwipeItem.name]: SwipeItem,
     [Tag.name]: Tag,
     [GoodsAction.name]: GoodsAction,
-    [GoodsActionBigBtn.name]: GoodsActionBigBtn
+    [GoodsActionBigBtn.name]: GoodsActionBigBtn,
+    [Progress.name]: Progress,
+    [Cell.name]: Cell,
+    [Steps.name]: Steps,
+    [Step.name]: Step,
+    [Actionsheet.name]: Actionsheet,
+    [Stepper.name]: Stepper,
+    [Button.name]: Button,
+    [Toast.name]: Toast
+  },
+  methods: {
+    onClickHistory() {
+      console.log('history')
+    },
+
+    handleBetClick() {
+      this.showPanel = true
+    },
+
+    submit() {
+      Toast.success('投注成功')
+    }
   },
   created() {
     this.$bus.$emit('hide-footer')
@@ -86,31 +162,39 @@ export default {
   font-weight: 600;
 }
 .good-desc {
-  margin: 10px 0;
+  margin: 10px 0 20px;
   font-size: 14px;
 }
-.good-tag {
-  margin-right: 5px;
+.good-progress {
+  margin-bottom: 20px;
+}
+.treasure-info-panel {
+  display: flex;
+  padding: 10px;
+  background: #e1e1e1;
+}
+.treasure-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1 1 auto;
+}
+.treasure-countdown {
+  border-left: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+}
+.treasure-history {
   margin-bottom: 10px;
-  font-size: 14px;
+  box-shadow: 3px 3px 3px #ccc;
 }
-.recommend-good-list {
-  padding: 15px;
-  background: #fff;
+.btn-bottom {
+  z-index: 10;
+}
+.bet-panel {
   font-size: 16px;
 }
-.recommend-title {
-  margin-bottom: 10px;
-  font-weight: 600;
-}
-.recommend-goods {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-}
-.recommend-image {
-  width: 48%;
-  height: 150px;
-  margin-bottom: 10px;
+.bet-form {
+  padding: 20px;
 }
 </style>
