6f2d247317b6ee2dea820d486c247c18df6c2b1d
samhanx <xuanxinhan@vip.qq.com>
2018-05-26 21:10:34 +0800

    feat: 订单列表页准备

---
+++ src/pages/OrderList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header-nav title="我的订单" />
+  </div>
+</template>
+
+<script>
+import HeaderNav from '@/components/HeaderNav'
+import api from '@/api'
+const { user } = api
+
+export default {
+  components: {
+    HeaderNav
+  },
+
+  created() {
+    user.getOrderList().then(res => {
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
